74c580da1723d2a279ef6b464c6dffd63b611e33
trygvanwyk <63888704+trygvanwyk@users.noreply.github.com>
2020-04-21 14:54:18 -0500

    added js

---
+++ JSfunctions.js
@@ -0,0 +1,6 @@
+function changeParagraph() {
+  document.getElementById("p1").innerHTML = "Paragraph changed.";
+}
+function revertParagraph() {
+  document.getElementById("p1").innerHTML = "Change this paragraph."
+}
